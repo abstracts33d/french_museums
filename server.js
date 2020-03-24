e430b3248e734cbccc1c64b1482c9ca666cab279
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-24 10:28:02 +0100

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 9000
 app.use(compression());
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'build')));
 
